fa9953ee828ae11eb9eec7fd5e21623413ba628f
Andrew Capshaw <andrew.capshaw@gmail.com>
2014-01-12 10:14:54 -0600

    Converge two angular apps into one

--- static.py
+++ static.py
@@ -5,7 +5,4 @@ static_api = Blueprint('static_api', __name__)
 
 @static_api.route('/')
 def anon_app():
-    if is_session_current():
-        return render_template('mainApp.html')
-    else:
-        return render_template('anonApp.html')
+    return render_template('app.html')
